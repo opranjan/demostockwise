28498d12e429c33ea3e8244d765eef7224fae0fb
opranjan <opranjan91700@gmail.com>
2026-04-24 09:49:10 +0530

    added config

---
+++ config/company.js
@@ -0,0 +1,27 @@
+const COMPANY = {
+  NAME: "StockwiseIq",
+  WEBSITE: "https://stockwiseiq.in/",
+
+  SEBI: {
+    REG_NO: "INH000018920",
+  },
+
+    ADDRESS: {
+    CITY_STATE: "Mumbai, Maharashtra, India",
+  },
+
+   GST: {
+    NUMBER: "27ALIPA1269N1ZW", // ← replace with real GST number
+  },
+
+  CONTACT: {
+    SUPPORT_EMAIL: "info@stockwiseiq.in",
+    REPORT_MOBILE: "+919619058820",
+  },
+
+  AGREEMENT: {
+    BASE_URL: "https://stockwiseiq.in/agreement",
+  },
+};
+
+module.exports = COMPANY;
